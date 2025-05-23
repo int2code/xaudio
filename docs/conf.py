713b16124488a5dcd34f08167d31bd9bb4791524
KamilPolok <kamil.polok@int2code.com>
2025-05-23 11:53:10 +0200

    CQ conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,41 +4,44 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 # pylint: skip-file
 from datetime import date
+
 from pkg_resources import get_distribution
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'xaudio'
+project = "xaudio"
 copyright = f"{date.today().year}, int2code"
-author = 'int2code'
+author = "int2code"
 version = release = get_distribution("xaudio").version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinxcontrib.programoutput',
-    'myst_parser'
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinxcontrib.programoutput",
+    "myst_parser",
 ]
 autosummary_generate = True
 
 # uncomment when _templates won't be empty
 # templates_path = ['._templates']
 exclude_patterns = [
-    '_build', 'Thumbs.db', '.DS_Store', 
-    'README.md'  # this is readme describing how to generate docs
-    ]
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "README.md",  # this is readme describing how to generate docs
+]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # uncomment when static content is added
 # html_static_path = ['_static']
 
